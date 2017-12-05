5c677ffc601847bbb51d117611c8fdf4baf17622
makuepfer <Mauro_Kuepfer@sluz.ch>
2017-12-05 13:43:27 +0100

    klassen match und teilnehmer bereitgestellt

---
+++ Turnierdirektion/Turnierdirektion/Match.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turnierdirektion
+{
+    public class Match
+    {
+        public Match(Teilnehmer teilnehmer1, int punkte1, Teilnehmer teilnehmer2, int punkte2)
+        {
+            this.Teilnehmer1 = teilnehmer1;
+            this.Teilnehmer2 = teilnehmer2;
+            this.Punkte1 = punkte1;
+            this.Punkte2 = punkte2;
+        }
+
+        public Teilnehmer Teilnehmer1 { get; set; }
+
+        public int Punkte1 { get; set; }
+
+        public Teilnehmer Teilnehmer2 { get; set; }
+
+        public int Punkte2 { get; set; }
+    }
+}
---
+++ Turnierdirektion/Turnierdirektion/Teilnehmer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turnierdirektion
+{
+    public class Teilnehmer
+    {
+        public Teilnehmer(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+        public int Punkte { get; set; }
+    }
+}
